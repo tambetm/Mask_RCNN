daa8785be67cf3ca2c46fa17e8c8ce969138ecb0
Tambet Matiisen <tambet.matiisen@gmail.com>
2018-03-02 17:27:44 +0200

    Simple server that returns JSON.

--- server.py
+++ server.py
@@ -1,11 +1,12 @@
 import argparse
-import os
+import tensorflow as tf
+import keras.backend as K
 import coco
 import model as modellib
+from flask import Flask, request, jsonify
 import skimage
 import skimage.measure
 import numpy as np
-import json
 
 # COCO Class names
 # Index of the class in the list is its ID. For example, to get ID of
@@ -27,7 +28,8 @@ CLASS_NAMES = ['BG', 'person', 'bicycle', 'car', 'motorcycle', 'airplane',
                'teddy bear', 'hair drier', 'toothbrush']
 
 parser = argparse.ArgumentParser()
-parser.add_argument('image_file')
+parser.add_argument('--host', default='0.0.0.0')
+parser.add_argument('--port', type=int, default=5000)
 parser.add_argument('--pretrained_weights', default='mask_rcnn_coco.h5')
 parser.add_argument('--verbose', action='store_true')
 args = parser.parse_args()
@@ -37,6 +39,12 @@ class InferenceConfig(coco.CocoConfig):
     # one image at a time. Batch size = GPU_COUNT * IMAGES_PER_GPU
     GPU_COUNT = 1
     IMAGES_PER_GPU = 1
+
+# to allow several web processes to share GPU memory
+config = tf.ConfigProto()
+config.gpu_options.allow_growth=True
+sess = tf.Session(config=config)
+K.set_session(sess)
 
 config = InferenceConfig()
 if args.verbose:
@@ -48,35 +56,42 @@ model = modellib.MaskRCNN(mode="inference", model_dir='.', config=config)
 # Load weights trained on MS-COCO
 model.load_weights(args.pretrained_weights, by_name=True)
 
-image = skimage.io.imread(args.image_file)
+app = Flask(__name__)
+#app.config['UPLOAD_FOLDER'] = 'uploads'
 
-# Run detection
-results = model.detect([image], verbose=int(args.verbose))
+@app.route("/detect", methods=['POST'])
+def detect():
+    assert 'image' in request.files
+    image_file = request.files['image']
+    image = skimage.io.imread(image_file)
 
-# Visualize results
-r = results[0]
-#visualize.display_instances(image, r['rois'], r['masks'], r['class_ids'], CLASS_NAMES, r['scores'])
+    # Run detection
+    results = model.detect([image], verbose=int(args.verbose))
+    r = results[0]
 
-outputs = []
-masks = np.moveaxis(r['masks'], 2, 0)
-for roi, mask, class_id, score in zip(r['rois'], masks, r['class_ids'], r['scores']):
-    # Mask Polygon
-    # Pad to ensure proper polygons for masks that touch image edges.
-    padded_mask = np.zeros(
-        (mask.shape[0] + 2, mask.shape[1] + 2), dtype=np.uint8)
-    padded_mask[1:-1, 1:-1] = mask
-    contours = skimage.measure.find_contours(padded_mask, 0.5)
-    # Subtract the padding and flip (y, x) to (x, y)
-    contours = [(np.fliplr(verts) - 1).tolist() for verts in contours]
-    
-    output = {
-        'roi': roi.tolist(),
-        #'mask': mask.tolist(),
-        'contours': contours,
-        'class_id': int(class_id),
-        'class': CLASS_NAMES[class_id],
-        'score': float(score)
-    }
-    outputs.append(output)
+    outputs = []
+    masks = np.moveaxis(r['masks'], 2, 0)
+    for roi, mask, class_id, score in zip(r['rois'], masks, r['class_ids'], r['scores']):
+        # Mask Polygon
+        # Pad to ensure proper polygons for masks that touch image edges.
+        padded_mask = np.zeros(
+            (mask.shape[0] + 2, mask.shape[1] + 2), dtype=np.uint8)
+        padded_mask[1:-1, 1:-1] = mask
+        contours = skimage.measure.find_contours(padded_mask, 0.5)
+        # Subtract the padding and flip (y, x) to (x, y)
+        contours = [(np.fliplr(verts) - 1).tolist() for verts in contours]
+        
+        output = {
+            'roi': roi.tolist(),
+            #'mask': mask.tolist(),
+            'contours': contours,
+            'class_id': int(class_id),
+            'class': CLASS_NAMES[class_id],
+            'score': float(score)
+        }
+        outputs.append(output)
 
-print(json.dumps(outputs))
+    return jsonify(outputs)
+
+if __name__ == '__main__':
+    app.run(args.host, args.port)
